0a96ded6f9479769df08fd30de507fffc062ae64
Dragt <clerbois@yahoo.fr>
2019-12-28 20:39:45 +0100

    met le css parchemin

--- styleClairEvenements.user.js
+++ styleClairEvenements.user.js
@@ -9,5 +9,21 @@
 
 
 if (window.location.pathname === "/mountyhall/View/PJView_Events.php") {
+  const cssBase = document.querySelector('link[href="/mountyhall/MH_PageUtils/MH_Style_ProfilSimple.css"]');
+  if (cssBase) {
+    document.querySelector('link[href*="//blason.mountyhall.com//Css_PJ"]').disabled = true;
+    cssBase.href = "/mountyhall/MH_Packs/packMH_parchemin/css/MH_Style_Play.css";
+  }
+  else if (document.querySelector('link[href="/mountyhall/MH_PageUtils/MH_Style_ProfilAvance.css"]')) {
+    document.querySelector('link[title*="Style personna"]').href = "/mountyhall/MH_PageUtils/MH_Style_ProfilAvance.css";
+  }
+}
+
+
+
+
+/* version simple
+if (window.location.pathname === "/mountyhall/View/PJView_Events.php") {
   window.addEventListener("load", function () {setActiveStyleSheet("Style de base");});
 }
+*/
