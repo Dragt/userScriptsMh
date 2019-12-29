c90656646111d89bfacca517cf4c2e80bd9b2bcb
Dragt <clerbois@yahoo.fr>
2019-12-29 10:10:28 +0100

    cas avec changements de base de couleurs

--- styleClairEvenements.user.js
+++ styleClairEvenements.user.js
@@ -11,7 +11,15 @@
 if (window.location.pathname === "/mountyhall/View/PJView_Events.php") {
   const cssBase = document.querySelector('link[href="/mountyhall/MH_PageUtils/MH_Style_ProfilSimple.css"]');
   if (cssBase) {
-    document.querySelector('link[href*="//blason.mountyhall.com//Css_PJ"]').disabled = true;
+    const cssBlason = document.querySelector('link[href*="//blason.mountyhall.com//Css_PJ"]');
+    if (cssBlason) {
+      cssBlason.disabled = true;
+    }
+    else {
+      Array.from(document.querySelectorAll("style"))
+        .filter(x => (trouves = x.innerText.match(/.mh_tdborder|.mh_tdtitre|.mh_tdpage|.mh_texte/g)) ? trouves.length === 4 : false)
+        .forEach(x => {x.innerText = ""});
+    }
     cssBase.href = "/mountyhall/MH_Packs/packMH_parchemin/css/MH_Style_Play.css";
   }
   else if (document.querySelector('link[href="/mountyhall/MH_PageUtils/MH_Style_ProfilAvance.css"]')) {
